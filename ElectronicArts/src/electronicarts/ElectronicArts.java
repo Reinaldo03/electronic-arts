bcee30b879ab8f6dbc6716897e6b8032e9a3483f
Angex43 <142070711+Angex43@users.noreply.github.com>
2023-11-01 18:00:36 -0300

    modificacion 2

--- ElectronicArts/src/electronicarts/ElectronicArts.java
+++ ElectronicArts/src/electronicarts/ElectronicArts.java
@@ -14,7 +14,7 @@ public class ElectronicArts {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
